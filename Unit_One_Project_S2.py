9b06ae3e27e4fc1e4d66d1ce652c47378a2badea
akiskowitz <iskowan@ssfs.org>
2022-09-13 08:21:26 -0400

    commit save_11

--- Unit_One_Project_S2.py
+++ Unit_One_Project_S2.py
@@ -5,34 +5,32 @@ import turtle
 # setting speed of turtle
 # turtle.speed(100)
 # defining "makeAnOctogon" and "moveTurtle"
-def makeAnOctogon(size, color):
-    turtle.color(color)
-    turtle.begin_fill()
-    for x in range(8):
-        turtle.fd(size)
-        turtle.right(45)
-    turtle.end_fill()
-
 def moveTurtle():
     turtle.penup()
     turtle.forward(100)
     turtle.left(90)
     turtle.pendown()
 
+def makeAnOctogon(size, color):
+    turtle.color(color)
+    turtle.begin_fill()
+        turtle.fd(size)
+    for x in range(8):
+        turtle.right(45)
+    turtle.end_fill()
+    moveTurtle()
+
+
 # octagon 1
 makeAnOctogon(50, "dark sea green")
-moveTurtle()
 
 #octagon 2
 makeAnOctogon(25, "orange red")
-moveTurtle()
 
 # octagon 3
 makeAnOctogon(40, "royal blue")
-moveTurtle()
 
 # octagon 4
 makeAnOctogon(30, "yellow green")
-moveTurtle()
 
 turtle.exitonclick()
